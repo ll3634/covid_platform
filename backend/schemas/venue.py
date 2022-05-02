49b3806ede7631954d832e434fb3cc5f23df03c4
wnhllh <839390202@qq.com>
2022-05-02 00:10:53 -0400

    venue info

--- backend/schemas/venue.py
+++ backend/schemas/venue.py
@@ -5,6 +5,12 @@ class VenueSchema(ma.SQLAlchemySchema):
         fields = ('id', 'name', 'description', 'categories', 'latitude', 'longitude', 'image_num', 'created_at')      
     categories = ma.Function(lambda obj: [category.name for category in obj.categories])
 
+
+class VenueInfoSchema(ma.SQLAlchemySchema):
+    class Meta:
+        fields = ('id', 'venue_id', 'capacity', 'start_hour', 'end_hour', 'created_at')      
+        
         
 venue_schema = VenueSchema()
 venues_schema = VenueSchema(many=True)
+venuesinfo_schema = VenueInfoSchema(many=True)

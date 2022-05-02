16917545a08ab19d5194df1854b65d74fe484adc
wnhllh <839390202@qq.com>
2022-05-01 22:31:57 -0400

    authentified navigator

--- .history/frontend/screens/password/actionType_20220501150518.js
+++
@@ -1,8 +0,0 @@
-const SET_SMS = 'login/SET_SMS'
-const SET_PHONE = 'login/SET_PHONE'
-const SET_PASSWORD = 'login/SET_PASSWORD'
-const SET_
-const SET_TOKEN = 'login/SET_TOKEN'
-const SET_VERIFY = 'login/SET_VERIFY'
-
-export { SET_SMS, SET_PHONE, SET_PASSWORD, SET_TOKEN, SET_VERIFY }
--- .history/frontend/screens/password/actionType_20220501150531.js
+++
@@ -1,8 +0,0 @@
-const SET_SMS = 'login/SET_SMS'
-const SET_PHONE = 'login/SET_PHONE'
-const SET_PASSWORD = 'login/SET_PASSWORD'
-const SET_CONFIRM = 'login/SET_CONFIRM'
-const SET_TOKEN = 'login/SET_TOKEN'
-const SET_VERIFY = 'login/SET_VERIFY'
-
-export { SET_SMS, SET_PHONE, SET_PASSWORD, SER_CONFIRM, SET_TOKEN, SET_VERIFY }
